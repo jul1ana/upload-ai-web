7ca053992374e4061cd5ab9e8ea2feaeaefab5a2
jul1ana <julianaadt97@gmail.com>
2023-09-14 18:55:47 -0300

    add transcricoes da IA

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
 import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <header className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
+        <h1 className="text-xl font-bold text-green-600">upload.ai</h1>
 
         <div className="flex items-center gap-2">
-          <span className="text-sm text-muted-foreground">
+          <span className="text-sm text-muted-foreground mr-3">
             Desenvolvido por Juliana Dantas
           </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
           <Button variant="link">
             <Github className="w-4 h-4 mr-1" />
             Github
           </Button>
         </div>
       </header>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-green-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80"></aside>
+      </main>
     </div>
   );
 }
