c9497d042508d39cf4e08ec678a1270621d1d78a
jul1ana <julianaadt97@gmail.com>
2023-09-14 16:47:47 -0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <>
-      <Button>TESTE</Button>
-    </>
+    <div>
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-2">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido por Juliana Dantas
+          </span>
+          <Button variant="link">
+            <Github className="w-4 h-4 mr-1" />
+            Github
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
